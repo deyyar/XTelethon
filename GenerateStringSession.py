77d80ce30127bb4a98652a59281c150a6495aee0
Shrimadhav U K <shrimadhavuk@gmail.com>
2020-06-09 22:12:42 +0530

    🤮🤮 make it easier to copy session string 😐

--- GenerateStringSession.py
+++ GenerateStringSession.py
@@ -10,9 +10,16 @@ from telethon.sessions import StringSession
 print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
-Create a new application, by entering the required details""")
+Create a new application, by entering the required details
+
+if Telegram is blocked by your ISP, you can try the @useTGxBot""")
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
-    print(client.session.save())
+    session_string = client.session.save()
+    saved_messages_template = """@UniBorg 
+<code>HU_STRING_SESSION</code>: <code>{}</code>
+
+⚠️ <i>it is forbidden to pass this value to third parties</i>""".format(session_string)
+    client.send_message("me", saved_messages_template, parse_mode="html")
