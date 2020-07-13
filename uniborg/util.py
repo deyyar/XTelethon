1ceb804c3c038033e1faf699c338cf5f8467b68e
Shrimadhav U K <SpEcHIDe@users.noreply.github.com>
2020-07-13 19:24:09 +0530

    fix is_admin condition check

--- uniborg/util.py
+++ uniborg/util.py
@@ -148,6 +148,8 @@ def time_formatter(milliseconds: int) -> str:
 
 
 async def is_admin(client, chat_id, user_id):
+    if not str(chat_id).startswith("-100"):
+        return False
     req_jo = await client(GetParticipantRequest(
         channel=chat_id,
         user_id=user_id
