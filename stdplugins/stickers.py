c3dd86618860b496d3e1de0e91550d15bd8fb058
Shrimadhav U K <SpEcHIDe@users.noreply.github.com>
2020-08-01 22:16:48 +0530

    stickers: fix too many stickers

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -42,7 +42,6 @@ async def _(event):
     if input_str:
         sticker_emoji = input_str
 
-    me = borg.me
     userid = borg.uid
     packname = f"{userid}'s @UniBorg Pack"
     packshortname = f"Uni_Borg_{userid}"  # format: Uni_Borg_userid
@@ -55,7 +54,7 @@ async def _(event):
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{userid}'s @AnimatedStickersGroup"
-        packshortname = f"Uni_Borg_{userid}_as"  # format: Uni_Borg_userid
+        packshortname = f"Uni_Borg_{userid}_as"  # format: Uni_Borg_userid_as
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
@@ -83,7 +82,7 @@ async def _(event):
             if not response.text.startswith("Alright!"):
                 await event.edit(f"**FAILED**! @Stickers replied: {response.text}")
                 return
-            w = await bot_conv.send_file(
+            await bot_conv.send_file(
                 file=uploaded_sticker,
                 allow_cache=False,
                 force_document=True
@@ -103,7 +102,12 @@ async def _(event):
         else:
             await silently_send_message(bot_conv, "/cancel")
             await silently_send_message(bot_conv, "/addsticker")
-            await silently_send_message(bot_conv, packshortname)
+            esnopre = await silently_send_message(bot_conv, packshortname)
+            if "Alright!" not in esnopre.text:
+                await event.edit(
+                    f"**FAILED**! @Stickers replied: {esnopre.text}"
+                )
+                return
             await bot_conv.send_file(
                 file=uploaded_sticker,
                 allow_cache=False,
